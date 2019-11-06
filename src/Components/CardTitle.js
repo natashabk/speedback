38108a456c0bdafb8e8b20d7f0c1bc6f9cb6cf10
Natasha Buck <natasha@Natashas-MacBook-Pro.local>
2019-11-06 08:56:27 +0000

    removed unnecessary component

--- src/Components/CardTitle.js
+++ src/Components/CardTitle.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Row, Typography } from 'antd';
+import { Row } from 'antd';
 import BackButton from './BackButton';
 import { pageInstructions } from '../Constants';
 import { useSessionValue } from '../SessionContext';
-
-const { Text } = Typography;
 
 const CardTitle = ({ timeRunning }) => {
 	const { active, isLastRound } = useSessionValue();
@@ -17,9 +15,7 @@ const CardTitle = ({ timeRunning }) => {
 	return (
 		<Row style={{ height: '10%', textAlign: 'center' }}>
 			{active !== 'Settings' && <BackButton />}
-			<Text>
-				{active === 'Round' ? roundText() : pageInstructions[active].title}
-			</Text>
+			{active === 'Round' ? roundText() : pageInstructions[active].title}
 		</Row>
 	);
 };
